eee88117ca107912f4e77171f69c6e9870fa107b
acdsadcx <nicolaas78pun0suy@hotmail.com>
2024-05-09 20:14:21 +0700

    start commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const randomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
+function levelOrder(root) {
+  if (!root) return [];
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      currentLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(currentLevel);
+  }
+  return result;
+}
